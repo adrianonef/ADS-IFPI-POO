095e8a909ac6362f44022c9a2c1247d0c928f79b
Adriano NEF <adrianonef@hotmail.com>
2016-01-06 16:56:45 -0300

    06 Jan 2016

---
+++ Exercicio05jan2016/Funcionario.java
@@ -0,0 +1,13 @@
+/*
+ * IFPI - ADS - 2015.2
+ * 2011.2ADS0019 Adriano José Sousa Santos  * 
+ */
+package Exercicio05jan2016;
+
+/**
+ *
+ * @author adrianonef
+ */
+public class Funcionario {
+    
+}
